68a626a189f8a03b6eb5bdae36b0ea4291305f57
Block <proplaya143@gmail.com>
2022-08-26 09:00:03 -0700

    Added UWeaponComponent

---
+++ Source/ProjectRadiate/Private/Weapon/WeaponComponent.cpp
@@ -0,0 +1,95 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapon/WeaponComponent.h"
+
+// Sets default values for this component's properties
+UWeaponComponent::UWeaponComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+    gunBody = CreateDefaultSubobject<USkeletalMesh>(TEXT("Gun"));
+
+    
+	// ...
+}
+
+
+// Called when the game starts
+void UWeaponComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	currentAmmoAmount = ammoCapacity;
+	// ...
+	
+}
+
+
+// Called every frame
+void UWeaponComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    if (currentReloadCooldown > 0.0f && bIsReloading)
+    {
+        currentReloadCooldown -= DeltaTime;
+    }
+    else if (currentReloadCooldown <= 0.0f && bIsReloading)
+    {
+        bIsReloading = false;
+    }
+    if (currentPerBulletCooldown > 0.0f && !bIsReloading)
+    {
+        currentPerBulletCooldown -= DeltaTime;
+    }
+    else if (currentReloadCooldown <= 0.0f && bIsReloading)
+    {
+        currentAmmoAmount = ammoCapacity;
+        currentReloadCooldown = reloadCooldown;
+        bIsReloading = false;
+    }
+}
+
+void UWeaponComponent::Shoot()
+{
+    if (currentAmmoAmount > 0 && currentPerBulletCooldown <= 0.0f)
+    {
+        if (bullet)
+        {
+            ABulletActor* newBullet = NewObject<ABulletActor>(TEXT("Bullet"));
+            if (newBullet)
+            {
+                currentAmmoAmount -= 1.0f;
+                newBullet.transform.position = barrelEnd.GetLocation();
+                newBullet.transform.rotation = barrelEnd.GetRotation();
+                //Debug.Log(barrelEnd.gameObject.transform.rotation.eulerAngles);
+                newBullet.SetTrajectory(barrelEnd.GetLocation());
+                currentPerBulletCooldown = maxPerBulletCooldown;
+                // newBullet.Launch();
+            }
+            else
+            {
+                //Translate
+                //Debug.LogError("BULLET NOT CREATED.");
+            }
+        }
+        else
+        {
+            //TRanslate
+           // Debug.LogError("BULLET DOES NOT EXIST");
+        }
+    }
+}
+
+void UWeaponComponent::Reload()
+{
+    if (currentAmmoAmount < ammoCapacity)
+    {
+        bIsReloading = true;
+        currentReloadCooldown = reloadCooldown;
+
+    }
+}
+
---
+++ Source/ProjectRadiate/Public/Weapon/WeaponComponent.h
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "WeaponComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class PROJECTRADIATE_API UWeaponComponent : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UWeaponComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	float currentAmmoAmount = 0;
+	float currentReloadCooldown = 0.0f;
+	bool bIsReloading = false;
+	float currentPerBulletCooldown = 0.0f;
+	USkeletalMeshComponent* gunBody;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	ABulletActor bullet;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		float ammoCapacity	= 0.0f;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		float damageOverride = 0.0f;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		float speedOverride = 0.0f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		float reloadCooldown = 0.0f;
+		
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		float maxPerBulletCooldown = 0.0f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		FTransform barrelEnd;
+
+	UFUNCTION()
+	virtual void Shoot();
+	UFUNCTION()
+	virtual void Reload();
+};
